0009424bda162344c25e44c697c647de3afdae06
pratikpatil0511 <pratikpatil0511@gmail.com>
2023-07-28 08:22:28 +0530

    wrote Junit test case with Mockito for deleteUser method of Service layer

--- src/test/java/com/bikkadit/electronicstore/service/UserServiceTest.java
+++ src/test/java/com/bikkadit/electronicstore/service/UserServiceTest.java
@@ -3,15 +3,21 @@ package com.bikkadit.electronicstore.service;
 import com.bikkadit.electronicstore.dto.UserDto;
 import com.bikkadit.electronicstore.entity.User;
 import com.bikkadit.electronicstore.repository.UserRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 
@@ -87,5 +93,27 @@ public class UserServiceTest
         Assertions.assertEquals("I am a Software Engineer",updated.getAbout());
 
         Assertions.assertEquals(userDto.getName(),updated.getName(),"test cast failed : not updated");
+    }
+
+    @SneakyThrows
+    @Test
+    public void deleteUserTest()
+    {
+        User user1 = User.builder()
+                .name("Pratik")
+                .email("pratik@gmail.com")
+                .gender("male")
+                .about("I am a Mechanical Engineer")
+                .imageName("pratik.png")
+                .password("Pratik@123")
+                .build();
+
+        String userId="abcd";
+
+        Mockito.when(userRepository.findById("abcd")).thenReturn(Optional.of(user1));
+
+        userService.deleteUser(userId);
+
+        Mockito.verify(userRepository,Mockito.times(1)).delete(user1);
     }
 }
